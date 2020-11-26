237800327ba62c42aa772552ac235750afc8de75
Henry Han <henry.hans.cc@gmail.com>
2020-11-26 12:02:46 -0800

    more changes

--- topnbuzzwords/main.cpp
+++ topnbuzzwords/main.cpp
@@ -2,6 +2,66 @@
 #include <sys/time.h>
 #include <bits/stdc++.h>
 
+// in quote example from cpp programming
+template <typename Container>
+bool in_quote(const Container& cont, const std::string& s)
+{
+    return std::search(cont.begin(), cont.end(), s.begin(), s.end()) != cont.end();
+}
+
+// search_n example from cpp programming
+template <class Container, class Size, class T>
+bool consecutive_values(const Container& c, Size count, const T& v)
+{
+  return std::search_n(std::begin(c),std::end(c),count,v) != std::end(c);
+}
+
+// the following is count
+//  // counting elements in array:
+//  int myints[] = {10,20,30,30,20,10,10,20};   // 8 elements
+//  int mycount = std::count (myints, myints+8, 10);
+//  std::cout << "10 appears " << mycount << " times.\n";
+//
+//  // counting elements in container:
+//  std::vector<int> myvector (myints, myints+8);
+//  mycount = std::count (myvector.begin(), myvector.end(), 20);
+//  std::cout << "20 appears " << mycount  << " times.\n";
+
+
+//  std::count_if
+//
+//bool IsOdd (int i) { return ((i%2)==1); }
+//
+//int main () {
+//  std::vector<int> myvector;
+//  for (int i=1; i<10; i++) myvector.push_back(i); // myvector: 1 2 3 4 5 6 7 8 9
+//
+//  int mycount = count_if (myvector.begin(), myvector.end(), IsOdd);
+//  std::cout << "myvector contains " << mycount  << " odd values.\n";
+//
+//  return 0;
+//}
+
+
+// std::find_if and std::find_if_not
+//bool IsOdd (int i) {
+//  return ((i%2)==1);
+//}
+
+//int main () {
+//  std::vector<int> myvector;
+
+//  myvector.push_back(10);
+//  myvector.push_back(25);
+//  myvector.push_back(40);
+//  myvector.push_back(55);
+
+//  std::vector<int>::iterator it = std::find_if (myvector.begin(), myvector.end(), IsOdd);
+//  std::cout << "The first odd value is " << *it << '\n';
+
+//  return 0;
+//}
+
 void printOutStr(std::string str)
 {
   std::cout << ' ' << str;
